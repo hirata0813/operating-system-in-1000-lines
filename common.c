6b279c080f3b8f314b9b14ca69ce4de2e825a52c
hirata <hirata2024@s.okayama-u.ac.jp>
2026-05-18 08:27:10 +0900

    06終了．簡単な標準Cライブラリを実装

--- common.c
+++ common.c
@@ -2,6 +2,44 @@
 
 void putchar(char ch);
 
+// src から n バイト分を dst にコピーする関数
+void *memcpy(void *dst, const void *src, size_t n) {
+    uint8_t *d = (uint8_t *) dst;
+    const uint8_t *s = (const uint8_t *) src;
+    while (n--)
+        *d++ = *s++;
+    return dst;
+}
+
+void *memset(void *buf, char c, size_t n) {
+    uint8_t *p = (uint8_t *) buf;
+    while (n--)
+        *p++ = c;
+    return buf;
+}
+
+// src の文字列を dst にコピーする関数
+char *strcpy(char *dst, const char *src) {
+    char *d = dst;
+    while (*src)
+        *d++ = *src++;
+    *d = '\0';
+    return dst;
+}
+
+// s1 と s2 の文字列を比較する関数
+// s1 と s2 が同じ文字列なら 0 を返す
+int strcmp(const char *s1, const char *s2) {
+    while (*s1 && *s2) {
+        if (*s1 != *s2)
+            break;
+        s1++;
+        s2++;
+    }
+
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
+
 void printf(const char *fmt, ...) {
     va_list vargs;
     va_start(vargs, fmt);
