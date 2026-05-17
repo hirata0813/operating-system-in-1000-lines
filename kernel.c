070f7901dd073b6c1e5fa1f4ccdaa4e4cd569ee7
hirata <hirata2024@s.okayama-u.ac.jp>
2026-05-17 19:57:25 +0900

    05終了．printf()を実装

--- kernel.c
+++ kernel.c
@@ -1,8 +1,36 @@
+#include "kernel.h"
+#include "common.h"
+
 typedef unsigned char uint8_t;
 typedef unsigned int uint32_t;
 typedef uint32_t size_t;
 
 extern char __bss[], __bss_end[], __stack_top[];
+
+struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
+                       long arg5, long fid, long eid) {
+    register long a0 __asm__("a0") = arg0; // 指定したレジスタに値を入れる命令
+    register long a1 __asm__("a1") = arg1;
+    register long a2 __asm__("a2") = arg2;
+    register long a3 __asm__("a3") = arg3;
+    register long a4 __asm__("a4") = arg4;
+    register long a5 __asm__("a5") = arg5;
+    register long a6 __asm__("a6") = fid; // SBI の仕様として，a6 レジスタで，SBI のどの関数を利用するかを決める
+    register long a7 __asm__("a7") = eid; // SBI の仕様として，a7 レジスタで，SBI のどの拡張機能を利用するかを決める(選んだ拡張機能の中で，さらに a6 で関数を選ぶ)
+
+    // ecall を呼び出すと，CPU の動作モードが s-mode から m-mode に切り替わり，OpenSBI の処理ハンドラが呼び出される
+    // OpenSBI の処理が終わると，自動的に m-mode から s-mode に切り替わり，ecall の次の命令から処理が再開される
+    __asm__ __volatile__("ecall"
+                         : "=r"(a0), "=r"(a1)
+                         : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5),
+                           "r"(a6), "r"(a7)
+                         : "memory");
+    return (struct sbiret){.error = a0, .value = a1};
+}
+
+void putchar(char ch) {
+    sbi_call(ch, 0, 0, 0, 0, 0, 0, 1 /* Console Putchar */);
+}
 
 void *memset(void *buf, char c, size_t n) {
     uint8_t *p = (uint8_t *) buf;
@@ -12,9 +40,16 @@ void *memset(void *buf, char c, size_t n) {
 }
 
 void kernel_main(void) {
-    memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
+    const char *s = "\n\nHello World!\n";
+    for (int i = 0; s[i] != '\0'; i++) {
+        putchar(s[i]);
+    }
+    printf("\n\nHello %s\n", "World!");
+    printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
 
-    for (;;);
+    for (;;) {
+        __asm__ __volatile__("wfi");
+    }
 }
 
 __attribute__((section(".text.boot")))
